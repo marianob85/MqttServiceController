37b802536ced01443f6864c343550f2b6011b81f
Mariusz Brzeski <marianob85@gmail.com>
2023-02-23 22:08:49 +0100

    Modified   MqttClient.go Modified   main.go

--- MqttClient.go
+++ MqttClient.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -14,13 +15,16 @@ func NewMqttClient(config Config) MqttClient {
 	var mqttClient MqttClient
 
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("ssl://%s:%d", config.Server, config.Port))
+	opts.AddBroker(fmt.Sprintf("%s:%d", config.Server, config.Port))
 	opts.SetClientID(config.ClientID)
 	opts.SetUsername(config.UserName)
 	opts.SetPassword(config.Password)
 	opts.SetDefaultPublishHandler(mqttClient.messagePubHandler)
 	opts.OnConnect = mqttClient.connectHandler
 	opts.OnConnectionLost = mqttClient.connectLostHandler
+	opts.OnReconnecting = mqttClient.reconnectingHandler
+	opts.ConnectRetry = true
+	opts.ConnectRetryInterval = 1 * time.Second
 	mqttClient.client = mqtt.NewClient(opts)
 	if token := mqttClient.client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
@@ -33,10 +37,14 @@ func (o *MqttClient) messagePubHandler(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 }
 
+func (o *MqttClient) reconnectingHandler(client mqtt.Client, options *mqtt.ClientOptions) {
+	fmt.Printf("Connect attempt\n")
+}
+
 func (o *MqttClient) connectHandler(client mqtt.Client) {
 	fmt.Println("Connected")
 }
 
 func (o *MqttClient) connectLostHandler(client mqtt.Client, err error) {
-	fmt.Printf("Connect lost: %v", err)
+	fmt.Printf("Connect lost: %v\n", err)
 }
--- main.go
+++ main.go
@@ -2,26 +2,62 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
+	"os/signal"
+	"sync"
 )
 
 //topic := "/Mariano-Oscam/oscam/online"
 //topic := "/Mariano-Oscam/os/online"
 
+// https://guzalexander.com/2017/05/31/gracefully-exit-server-in-go.html
+
 func main() {
-	go terminationSetup()
 	var configuration = readConfiguration("config.json")
 	client := NewMqttClient(configuration)
-	for {
-		time.Sleep(1 * time.Second)
+
+	task := &Task{
+		closed: make(chan struct{}),
+		client: client,
 	}
 
-	//client.client.Disconnect(100)
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+
+	task.wg.Add(1)
+	go func() { defer task.wg.Done(); task.Run() }()
+
+	select {
+	case sig := <-c:
+		fmt.Printf("Got %s signal. Aborting...\n", sig)
+		task.Stop()
+	}
 }
 
-func sub(client MqttClient) {
-	topic := "/Mariano-Oscam/oscam/online"
-	token := client.client.Subscribe(topic, 1, nil)
-	token.Wait()
-	fmt.Printf("Subscribed to topic: %s", topic)
+type Task struct {
+	closed chan struct{}
+	wg     sync.WaitGroup
+	client MqttClient
 }
+
+func (t *Task) Run() {
+	for {
+		select {
+		case <-t.closed:
+			return
+		}
+	}
+}
+
+func (t *Task) Stop() {
+	t.client.client.Disconnect(100)
+	close(t.closed)
+	t.wg.Wait()
+}
+
+// func sub(client MqttClient) {
+// 	topic := "/Mariano-Oscam/oscam/online"
+// 	token := client.client.Subscribe(topic, 1, nil)
+// 	token.Wait()
+// 	fmt.Printf("Subscribed to topic: %s", topic)
+// }
